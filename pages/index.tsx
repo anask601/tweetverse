9d761343396704116ae71bb2821dd3ba0482b22a
anask601 <anask60155@gmail.com>
2023-08-28 01:04:40 +0530

    ✨ Create layout for tweetverse structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Hi there</div>;
+  return <div className="text-3xl text-sky-600">مرحبا بالعالم</div>;
 };
 
 export default Home;
